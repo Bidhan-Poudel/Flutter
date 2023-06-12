6b0f9914ef1327475cf0101f3f79fb844d94aefe
Bidhan Poudel <bidhanpoudel0@gmail.com>
2023-06-12 20:47:53 +0545

    First Commit

---
+++ lib/gradient_container.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+import 'package:first_app/text_style.dart';
+
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
+// class GradientContainer extends StatelessWidget {
+//   const GradientContainer(this.color,{super.key});
+
+//   final List<Color> color;
+
+//   @override
+//   Widget build(context) {
+//     return Container(
+//       decoration:  BoxDecoration(
+//         gradient: LinearGradient(
+//           colors: color,
+//           begin: startAlignment,
+//           end: endAlignment,
+//         ),
+//       ),
+//       child: const Center(
+//         child: StyledText("Hello World!"),
+//       ),
+//     );
+//   }
+// }
+
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer(this.color1,this.color2 ,{super.key});
+
+  final Color color1;
+  final Color color2;
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration:  BoxDecoration(
+        gradient: LinearGradient(
+          colors: [color1,color2],
+          begin: startAlignment,
+          end: endAlignment,
+        ),
+      ),
+      child: const Center(
+        child: StyledText("Hello World!"),
+      ),
+    );
+  }
+}
